e0782fb196af6c194a30cf2ded35900383722429
Wambati <165009303+Wambati@users.noreply.github.com>
2025-04-08 07:37:15 +0300

    Python Challenge 2

---
+++ 30_day_Challenge/Challenge2_Odd_Even_Game.py
@@ -0,0 +1,12 @@
+
+try:
+    num = int(input("Enter a number: "))
+except ValueError:
+    print("Invalid input\n")
+    num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(f"{num} is an even number")
+else:
+    print(f"{num} is an odd number")
+

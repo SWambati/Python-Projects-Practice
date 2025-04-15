57349b7cd9965fe0c739fbeab2fd261df4628586
Wambati <165009303+Wambati@users.noreply.github.com>
2025-04-15 07:47:34 +0300

    Python Challenge 5

--- 30_day_Challenge/Challenge5_Multiplication_Table_Generator.py
+++ 30_day_Challenge/Challenge5_Multiplication_Table_Generator.py
@@ -3,9 +3,10 @@
 num = int(input("Hello user. Kindly enter a number to get it's multiplication table: "))
 
 print(f"Multiplication table for {num} \n")
-for i in range(1, 21):
-    result = num * i
-    print(f"{num} x {i} = {result}")
+for n in range(1, 21):
+    result = num * n
+    print(f"{num} x {n} = {result}")
+
 
 
 

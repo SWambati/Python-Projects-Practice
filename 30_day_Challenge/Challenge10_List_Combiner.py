8e30f87f00658e7c6e451852905ab39f228a9e55
Wambati <165009303+Wambati@users.noreply.github.com>
2025-06-09 07:54:02 +0300

    Python Challenge 10

---
+++ 30_day_Challenge/Challenge10_List_Combiner.py
@@ -0,0 +1,13 @@
+#this program allows a user to enter a list, combines them and generates a final list with no duplicates
+
+input1 = input("Enter a list of values. Remember to space your values: ")
+input2 = input("Enter another list of values. Remember to space your values: ")
+
+list1 = input1.split()
+list2 = input2.split()
+
+combined_list = list1 + list2
+
+final_list = list(set(combined_list))
+
+print("The final combined list of unique values: ",final_list)

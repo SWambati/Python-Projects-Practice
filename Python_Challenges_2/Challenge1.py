5effe15a26a88261d5a817a40e28b0b0d7ef2b53
Wambati <165009303+Wambati@users.noreply.github.com>
2025-08-28 10:50:39 +0300

    User Input

---
+++ Python_Challenges_2/Challenge1.py
@@ -0,0 +1,10 @@
+#this is a basic python program that asks users for input
+
+print("Hello user")
+
+user = input("What is your name? ")
+
+hobby= input("What is your hobby? ")
+
+print(f"Hello, {user}. You like {hobby}")
+

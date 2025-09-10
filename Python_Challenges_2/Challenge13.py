2b3b7836cfea839dc4e324eab9a18ec813dab179
SWambati <sylvia.wambati@strathmore.edu>
2025-09-10 19:36:49 +0300

    Prime Numbers

---
+++ Python_Challenges_2/Challenge13.py
@@ -0,0 +1,22 @@
+#this program checks if a number is a prime number
+
+
+
+def prime_num(num):
+    if num <= 1:
+        return False
+    
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number: "))    
+if (prime_num(num)):
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
+
+
+
+
